dd1b548d7103f8842a25d83090d2400237b1a025
sudheervydadi <sudheervydadi@gmail.com>
2020-06-09 17:14:14 +0530

    Covariance funtion

--- CorrelationCoefficent.py
+++ CorrelationCoefficent.py
@@ -21,16 +21,13 @@ def correlation(X,Y):
     r=((n*np.sum(data['XY']))-(np.sum(data['X'])*np.sum(data['Y'])))/np.sqrt((n*np.sum(data['X^2'])-np.sum(data['X'])**2)*(n*np.sum(data['Y^2'])-np.sum(data['Y'])**2))
     
     return r
-'''
-X=np.array([43,21,25,42,57,59])
-Y=np.array([99,65,79,75,87,81])
 
-import plotly.graph_objects as go
-from plotly.offline import plot
-fig = go.Figure()
-
-fig.add_trace(go.Scatter(x=data['X'], y=data['Y'],mode='markers'))
-
-fig.update_layout()
-plot(fig, auto_open=True)
-'''
+def covariance(X,Y):
+    data=pd.DataFrame()
+    data['X']=pd.Series(data=X)
+    data['Y']=pd.Series(data=Y)
+    data['X-X_bar']=data['X']-np.mean(data['X'])
+    data['Y-Y_bar']=data['Y']-np.mean(data['Y'])
+    data['X-X_bar*Y-Y_bar']=data['X-X_bar']*data['Y-Y_bar']
+    
+    return np.sum(data['X-X_bar*Y-Y_bar'])/(len(data)-1)
